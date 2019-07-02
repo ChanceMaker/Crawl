2cc1b84b7f7748bddc06b372e0284419d7f5748f
Cedrick Baker <cedrickbaker7@gmail.com>
2019-07-02 00:53:51 -0400

    Working to page 21 in the rust programming book

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,38 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 
 fn main() {
-    println!("Guess the number");
-    println!("Please input your guess.");
-    let mut guess:String = String::new();
+    println!("Guess the number!");
 
-    io::stdin().read_line(&mut guess).expect("failed to read line");
-    println!("You guessed: {}",guess);
-    
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
+    loop{
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = guess.trim().parse().expect("Please Type a number");
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Less => println!("To Small!"),
+            Ordering::Equal => println!("You Win!"),
+        };
+    }
 
 }
+
+
+
+
 
 
